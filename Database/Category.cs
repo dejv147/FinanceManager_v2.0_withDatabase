fbf9511aee7f813006b380e5baa5949848d59646
David Halas <dejvu147@gmail.com>
2020-05-02 18:21:46 +0200

    Rename Category.cs to Database/Category.cs

---
+++ Database/Category.cs
@@ -0,0 +1,57 @@
+/// <summary>
+/// Aplikace pro správu financí určena pouze pro osobní užití.
+/// ----------------------------------------------------------
+/// Aplikace pracuje vždy pouze s daty patřící přihlášenému uživateli. 
+/// Data jsou uložena na lokálním databázovém serveru, kde jsou jednotlivé prvky uloženy v konkrétních tabulkách. 
+/// Díky Entity Frameworku jsou prvky z různých tabulek spojeny referencemi, díky tomu jem možné pracovat vždy pouze s daty přihlášeného uživatele, 
+/// kdy reference na konkrétního uživatele je v každé podmínce definující selekci (výběr) potřebných dat.
+/// Vždy při přihlášení se z databáze načte instance konkrétního uživatele, který je uchováván v kontroléru aplikace.
+/// 
+/// Aplikace implementuje zjednodušenou strukturu MVC architektury, kdy je aplikace rozdělena do 3 sekcí. 
+/// Třídy View jsou rozděleny na pohledy psané v XAML kódu a slouží pro zobrazení dat v okenním formuláři a třídy obsluhující dané pohledy, které slouží k nastavení okenních formulářů a načtení dat určených k zobrazení.
+/// Třídy Models jsou funkční třídy které uchovávají různé funkce a metody, které jsou využity pro zpracování dat, provedení různých úkonů pro zajištění správného chodu aplikace a předání dat určených k zobrazení uživateli.
+/// Třídy Controllers slouží k propojení pohledů a funkčních tříd. Zprostředkovává komunikaci, předávání dat a požadavků mezi jednotlivými třídami a uchovává metody pro zobrazování oken aplikace.
+/// 
+/// V hlavním okně aplikace je zobrazen stručný přehled a je zde uživatelské rozhraní pro správu aplikace i pro možnost využití dalších funkcí aplikace pracujících v samostatných oknech.
+/// V úvodu je otevřeno okno pro přihlášení uživatele a po úspěšném přihlášení je zobrazeno hlavní okno aplikace, které je stále otevřeno při chodu aplikace. Po zavření hlavního okna je aplikace ukončena.
+/// 
+/// 
+/// Autor projektu: Bc. David Halas
+/// Link publikovaného projektu: https://github.com/dejv147/FinanceManager_v2.0_withDatabase
+/// </summary>
+namespace SpravceFinanci_v2.Database
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Category")]
+    public partial class Category
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Category()
+        {
+            this.Items = new HashSet<Item>();
+            this.Transactions = new HashSet<Transaction>();
+        }
+    
+
+        public int Id { get; set; }
+
+         [Required]
+         [StringLength(50)]
+        public string Name { get; set; }
+
+      [Required]
+      [StringLength(100)]
+        public string Description { get; set; }
+    
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Item> Items { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Transaction> Transactions { get; set; }
+    }
+}
